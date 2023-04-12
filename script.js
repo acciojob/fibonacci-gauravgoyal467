2051adab80effd5d1d0b494c2377e7605dabfe58
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-04-12 23:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	let firstNum=0;
+	let secondNum=1;
+	let temp;
+	int ans=0;
+	if(num==1){
+		ans=firstNum;
+	}else if(num==2){
+		ans=secondNum;
+	}else{
+		for (let index = 3; index <= num; index++) {
+			temp=secondNum;
+			secondNum=firstNum+secondNum;
+			firstNum=temp;	
+		}
+		ans=secondNum;
+	}
+	return ans;
 }
 
 module.exports = fibonacci;
